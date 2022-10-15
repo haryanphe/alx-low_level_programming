c66d8e9bede09e99fcf19d679c5c47bd2458909b
haryanphe <olaseindeloveth@gmail.com>
2022-10-14 17:05:21 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-int puts(const char"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+/**
+ * main : Entry
+ * Return: Always 0
+ */
+
+int main (void)
+
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return(0);
+ 
+}
