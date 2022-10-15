b97bde0441e410c61556f5c953c7e03894965919
haryanphe <olaseindeloveth@gmail.com>
2022-10-15 07:22:45 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 /*
- * main- use puts and not printf
- * Return:always 0
+ *main- use puts and not printf
+ *
+ * Return: Always 0
  */
 
 int main(void)
 
 {
-
-puts("\ "Programming is like building a multilingual puzzle"); 
-return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
 }
-
