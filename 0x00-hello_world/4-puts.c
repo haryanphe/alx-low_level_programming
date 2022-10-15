185cbc46eed052cce2a01773742bcc80a07efeaa
haryanphe <olaseindeloveth@gmail.com>
2022-10-14 17:09:12 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * main : Entry
+ * main - Entry
  * Return: Always 0
  */
 
 int main (void)
 
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return(0);
+
+ puts("\"programming is like building a multilingual puzzle");
+ return (0);
  
 }
