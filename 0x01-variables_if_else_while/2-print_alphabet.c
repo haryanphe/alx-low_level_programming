281db3adf2420361fb971adab14b449b981456d1
haryanphe <olaseindeloveth@gmail.com>
2022-10-18 00:17:49 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - putchar alphabet in lower case
+ * return 0
+ */
+int main(void)
+{
+	char hc;
+
+	for
+		(hc = 'a'; hc <= 'z';
+		 hc++);
+	{
+	putchar(hc);
+	}
+	{
+	putchar('\n');
+	}
+
+	return (0);
+}
