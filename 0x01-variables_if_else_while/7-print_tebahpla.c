db9dc2d15d48f6050f7aaadc26cd0d587b95fb83
haryanphe <olaseindeloveth@gmail.com>
2022-10-18 03:33:22 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print lower case alphabet in reverse order
+ * Return : 0
+*/
+int main(void)
+{
+	char lc;
+
+	for (lc = 'z'; lc = 'a'; lc++)
+	{
+		putchar(lc);
+	}
+		putchar('\n');
+		return (0);
+}
