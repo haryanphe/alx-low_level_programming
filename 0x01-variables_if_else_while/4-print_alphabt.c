eaa9d03499a6b69a5bd836bb46034ebfdacb174b
haryanphe <olaseindeloveth@gmail.com>
2022-10-18 01:42:44 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print aplhabet a to z except q and e
+ * return:0
+*/
+int main(void)
+{
+	char bc;
+
+	for (bc = 'a'; bc = 'z'; bc++)
+	if (bc != 'q' && bc != 'e')
+	{
+	putchar(bc);
+	}
+	{
+	putchar('\n');
+	}
+	return(0);
+}
