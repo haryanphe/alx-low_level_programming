7848c79a0f98d238e7b1bcc14e6c77e55a267007
haryanphe <olaseindeloveth@gmail.com>
2022-10-19 10:02:55 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * main -Entry point
+  * function
+  *Return : 0
+  */
+int main(void)
+{
+	for (int x = 'a'; x <= 'z'; x++)
+{
+	int tolower_x = 'x';
+
+	_putchar('x');
+	_putchar('\n');
+}
+	return (0);
+}
