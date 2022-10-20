a27c5a8b571fdb06202695b9498a2896d4dc2e2a
haryanphe <olaseindeloveth@gmail.com>
2022-10-20 01:35:29 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,10 @@ void print_alphabet_x10(void)
 {
 	int xo;
 	int xi;
-
-	for (xi = '1'; xi <= '9'; xi++)
+	
 	for (xo = 'a'; xo <= 'z'; xo++)
+	for (xi = '1'; xi <= '9'; xi++)
+
 	_putchar(xo);
 	_putchar('\n');
 }
