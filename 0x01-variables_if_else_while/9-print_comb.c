b3fccbd3dc4349349a5009ca055f831901bbfbae
haryanphe <olaseindeloveth@gmail.com>
2022-10-23 09:01:43 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,16 +2,16 @@
 
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num < 9; num++)
 	{
 	    putchar(num);
-		if (num != 10)
+		if (num != 9)
 		{
 			putchar(',');
 			putchar(' ');
